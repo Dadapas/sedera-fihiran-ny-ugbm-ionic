4a4a40d32cb11c2fa4409248a99647df30ba62a7
TOVOHERY <tovoherypascal@gmail.com>
2016-02-22 21:19:59 +0300

    Del sedera img and add ng-repeat on collectio-repeat

--- www/js/app.js
+++ www/js/app.js
@@ -4,23 +4,68 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 var myApp = angular.module('sedera', ['ionic', 'ui.router', 'sedera.controllers', 'sedera.services'])
-	.run(function ($ionicPlatform) {
-            $ionicPlatform.ready(function () {
-                if (window.cordova && window.cordova.plugins.Keyboard) {
-                    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-                    // for form inputs)
-                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+	.run(function ($ionicPlatform, $cordovaDeviceOrientation) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-                    // Don't remove this line unless you know what you are doing. It stops the viewport
-                    // from snapping when text inputs are focused. Ionic handles this internally for
-                    // a much nicer keyboard experience.
-                    cordova.plugins.Keyboard.disableScroll(true);
-                }
-                if (window.StatusBar) {
-                    StatusBar.styleDefault();
-                }
-            });
-        });
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+    document.addEventListener("deviceready", function () {
+
+    $cordovaDeviceOrientation.getCurrentHeading().then(function(result) {
+      var magneticHeading = result.magneticHeading;
+      var trueHeading = result.trueHeading;
+      var accuracy = result.headingAccuracy;
+      var timeStamp = result.timestamp;
+    }, function(err) {
+      // An error occurred
+      console.log(err);
+    });
+
+
+
+    var options = {
+      frequency: 3000,
+      filter: true     // if frequency is set, filter is ignored
+    }
+
+    var watch = $cordovaDeviceOrientation.watchHeading(options).then(
+      null,
+      function(error) {
+        // An error occurred
+        console.log(error);
+      },
+      function(result) {   // updates constantly (depending on frequency value)
+        var magneticHeading = result.magneticHeading;
+        var trueHeading = result.trueHeading;
+        var accuracy = result.headingAccuracy;
+        var timeStamp = result.timestamp;
+      });
+
+
+    watch.clearWatch();
+    // OR
+    $cordovaDeviceOrientation.clearWatch(watch)
+      .then(function(result) {
+        // Success!
+        console.log(result);
+      }, function(err) {
+        // An error occurred
+        console.log(err);
+      });
+
+    }, false);
+  });
 
 myApp.config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,24 +1,15 @@
 angular.module('sedera.controllers', ['ngSanitize', 'ngCordova'])
+
 // Ici les messages de bienvenue dans l'application
 .controller('HomeCtrl', function ($scope) {
-    /*
-     * Ici c'est vraimment affichage de de bienvenue
-     */
+
 })
-    .controller('SederaCtrl', function ($scope, SederaFactory) {
-        $scope.sederas = SederaFactory.SomeSedera;
 
-        $scope.loadMore = function (){
-            $scope.sederas.push(SederaFactory.findOneByNum($scope.sederas.length));
-            $scope.$broadcast('scroll.infiniteScrollComplete');
-        }
+.controller('SederaCtrl', function ($scope, SederaFactory) {
+	$scope.sederas = SederaFactory.findAll();
+})
 
-        $scope.$on('$stateChangeSuccess', function () {
-            $scope.loadMore();
-        })
-    })
-
-.controller('HiraCtrl', function ($scope, SederaFactory, $stateParams) {
+.controller('HiraCtrl', function ($scope, $stateParams, SederaFactory) {
     $scope.Sedera = SederaFactory.findOneByNum($stateParams.num);
 })
 
@@ -28,55 +19,6 @@ angular.module('sedera.controllers', ['ngSanitize', 'ngCordova'])
      * ugbm
      * A propos du concepteur et réalisateur du projet
      */
-  $scope.version = "1.0.0@dev"
-})
-
-.controller('DiviceOrientationCtrl', function($cordovaDeviceOrientation) {
-
-  document.addEventListener("deviceready", function () {
-
-    $cordovaDeviceOrientation.getCurrentHeading().then(function(result) {
-       var magneticHeading = result.magneticHeading;
-       var trueHeading = result.trueHeading;
-       var accuracy = result.headingAccuracy;
-       var timeStamp = result.timestamp;
-    }, function(err) {
-      // An error occurred
-      console.log(err);
-    });
-
-
-
-    var options = {
-      frequency: 3000,
-      filter: true     // if frequency is set, filter is ignored
-    }
-
-    var watch = $cordovaDeviceOrientation.watchHeading(options).then(
-      null,
-      function(error) {
-        // An error occurred
-        console.log(error);
-      },
-      function(result) {   // updates constantly (depending on frequency value)
-        var magneticHeading = result.magneticHeading;
-        var trueHeading = result.trueHeading;
-        var accuracy = result.headingAccuracy;
-        var timeStamp = result.timestamp;
-      });
-
-
-    watch.clearWatch();
-    // OR
-    $cordovaDeviceOrientation.clearWatch(watch)
-      .then(function(result) {
-        // Success!
-        console.log(result);
-      }, function(err) {
-        // An error occurred
-        console.log(err);
-      });
-
-  }, false);
-})
-;
+  $scope.version = "1.0.0@dev",
+  $scope.angularVersion = angular.version;
+});
